699d3ce98fafe7257de7a42eb60eeef5fb21305b
Gabriel Pittol <gabrielpittol@hotmail.com>
2021-04-24 22:20:41 -0300

    Slime

--- Assets/Scripts/Gameplay/Enemies/IASlime.cs
+++ Assets/Scripts/Gameplay/Enemies/IASlime.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class IASlime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public EnemyController enemyController;
+    public Animator animator;
+    public BoxCollider2D slimeCollider;
+
+    public bool canMove;
+    public bool canSpeed;
+
+    public Vector2 positionToMoveTo;
+
+    public float currentDelay = 0;
+
+    private void Update()
     {
-        
+        currentDelay += Time.deltaTime;
+        if (currentDelay > enemyController.delayBetweenAttacks) EnableSpeed();
+
+        if(canMove)
+        {
+            animator.Play("SlimeJump");
+            if(canSpeed) transform.position = Vector2.MoveTowards(transform.position, positionToMoveTo, enemyController.speed * Time.deltaTime);
+            currentDelay = 0;
+        }
+        else
+        {
+            animator.Play("SlimeIdle");
+            positionToMoveTo = new Vector2(CharacterManager.instance.transform.position.x, CharacterManager.instance.transform.position.y - 0.5f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        print(collision.tag);
+
+        if (collision.CompareTag("Player"))
+        {
+            CharacterCollision.instance.PlayerTakeDamage(enemyController.baseDamage);
+        }
+    }
+
+    public void EnableSpeed()
+    {
+        canMove = true;
+    }
+
+    public void DisableSpeed()
+    {
+        canMove = false;
+    }
+
+    public void CanSpeed()
+    {
+        canSpeed = true;
+    }
+
+    public void CantSpeed()
+    {
+        canSpeed = false;
+    }
+    public void DisableBoxCollider()
+    {
+        slimeCollider.enabled = false;
+    }
+
+    public void enableBoxCollider()
+    {
+        slimeCollider.enabled = true;
     }
 }
